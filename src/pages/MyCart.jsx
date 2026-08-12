1ceaa084a33d404a6793d35587c86a0aaf45753d
AkashdeepArjun <wadhwaarjun007@gmail.com>
2026-08-12 13:29:33 +0530

    dunno wth i did

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -41,8 +41,11 @@ export default function MyCart(){
     }  */
 
 
-        const onDelete = async (cart_id, product_id) => {
+        const onDelete = async (e,cart_id, product_id) => {
     try {
+
+
+        e.preventDefault()
         const response = await delete_from_cart(cart_id, product_id);
         
         if (response.status === "ok") {
