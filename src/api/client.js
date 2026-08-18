56526711cb71726bc4a2d7a6bc61c1c3af9dc5f2
AkashdeepArjun <wadhwaarjun007@gmail.com>
2026-08-18 18:46:23 +0530

    added interceptor to acept data from form

--- src/api/client.js
+++ src/api/client.js
@@ -8,6 +8,23 @@ const api = axios.create({
         "Content-Type": "application/json"
 
     }
+
+
+
+
 });
+// Interceptor to handle FormData uploads seamlessly
+api.interceptors.request.use((config) => {
+  if (config.data instanceof FormData) {
+    // Delete the hardcoded json header so the browser sets multipart/form-data + boundary
+    delete config.headers['Content-Type'];
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+
+
 
 export default api
