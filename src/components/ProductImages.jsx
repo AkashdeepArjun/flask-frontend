50bddf1b774834782ce7b72a9ddc87a81f334486
AkashdeepArjun <wadhwaarjun007@gmail.com>
2026-08-19 15:15:57 +0530

    added details in grid

--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 
 import api from "../api/client"
 import { useParams,Link } from "react-router-dom"
+import { div } from "framer-motion/client"
 
 
 
@@ -38,6 +39,7 @@ export default function ProductImages() {
             if(status ==="ok"){
 
                 setImageUrls(images)
+                setPreviewImageUrl(images[0])
 
             }else{
 
@@ -76,31 +78,63 @@ export default function ProductImages() {
 
     return(
 
-        <div className={`w-0.7 h-0.6 ${are_images_loading ?'animate-pulse':''}   esabsolute top-4 grif grid-cols-2  grid-rows-1 w-screen bg-gray-200 border-slate-500 rounded-2xl`}>
+        <div className={`w-0.5 h-0.6 ${are_images_loading ?'animate-pulse':''}   esabsolute top-4 grif grid-cols-2  grid-rows-1 w-screen bg-gray-200 border-slate-500 rounded-2xl`}>
 
             
             {image_urls && (<div className="w-full max-h-full p-2 border border-b-cyan-100">
 
-                            <img src={image_urls[0]} alt="" />
+                            <img src={preview_image_url} alt="" />
                 
                 
                 
                         </div>
+
+        ) && (
+
+            <div className="w-full max-h-full p-2 grid grid-cols-3 grid-rows-3">
+
+               
+               {image_urls.map((l)=>(
+
+                <div className={`w-full h-full ${l.trim() ==preview_image_url.trim()?'border border-t-amber-400':''} `} onClick={(e)=>{
+                    
+                    e.preventDefault()
+                    setPreviewImageUrl(l)}}> 
+
+                    <img src={l} className="w-full h-full object-contain" />
+
+
+
+
+                </div>
+
+
+               ))}
+
+
+
+
+
+
+
+
+            </div>
+
+
+
+
+        )
         
         
         
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        )}
+        }
+
+            
+
+
 
 
 
